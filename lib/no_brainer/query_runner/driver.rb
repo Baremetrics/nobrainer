44729d290383dc0a97ce8ef3be196a2e56bf7ec0
Scott Robertson <scottymeuk@gmail.com>
2016-12-14 12:18:46 +0000

    Use our pool as a test

--- lib/no_brainer/query_runner/driver.rb
+++ lib/no_brainer/query_runner/driver.rb
@@ -2,6 +2,9 @@ class NoBrainer::QueryRunner::Driver < NoBrainer::QueryRunner::Middleware
   def call(env)
     options = env[:options]
     options = options.merge(:db => RethinkDB::RQL.new.db(options[:db])) if options[:db]
-    env[:query].run(NoBrainer.connection.raw, options)
+
+    RDB::Pool.instance.the_pool.with do |connection|
+      env[:query].run(connection, options)
+    end
   end
 end
