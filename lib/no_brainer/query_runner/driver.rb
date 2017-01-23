55a8b91b9cb28523160ace9da49c18c213099944
Scott Robertson <scottymeuk@gmail.com>
2017-01-23 17:13:32 +0000

    Track the entire thing in scout

--- lib/no_brainer/query_runner/driver.rb
+++ lib/no_brainer/query_runner/driver.rb
@@ -1,10 +1,26 @@
 class NoBrainer::QueryRunner::Driver < NoBrainer::QueryRunner::Middleware
   def call(env)
-    options = env[:options]
-    options = options.merge(:db => RethinkDB::RQL.new.db(options[:db])) if options[:db]
-
-    RDB::Pool.instance.with do |connection|
-      env[:query].run(connection, options)
+    instrument_metrics(env) do
+      RDB::Pool.instance.with do |connection|
+        options = env[:options]
+        options = options.merge(:db => RethinkDB::RQL.new.db(options[:db])) if options[:db]
+        
+        env[:query].run(connection, options)
+      end
     end
   end
+
+  include ScoutApm::Tracer
+
+  def instrument_metrics(env)
+    query = RethinkDB::RPP.pp(env[:query]).truncate(150)
+    table_regex = /(?<=table\(\")(.*?)(?=\")/
+    table_name = query[table_regex]
+
+    # ScoutApp
+    self.class.instrument("rethinkdb.nobrainer", table_name, desc: query) do
+      yield
+    end
+  end
+
 end
